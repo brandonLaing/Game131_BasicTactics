2b1acbe1e7b9e981e2ad58c40d0f6111696a0c6b
svc-student <svc-student@student.directory.jccc>
2019-04-02 19:21:32 -0500

    Worked on stuff

---
+++ Assets/Editor/ActorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Actor))]
+[CanEditMultipleObjects]
+public class ActorEditor : Editor
+{
+  bool showHealth = false;
+  int tempHolder = 0;
+
+  public override void OnInspectorGUI()
+  {
+    Actor actorScript = target as Actor;
+
+    actorScript.name = EditorGUILayout.TextField("Actor Name", actorScript.name);
+
+    showHealth = EditorGUILayout.Foldout(showHealth, $"Current Health: {actorScript.hitPoints}");
+
+    if (showHealth)
+    {
+      tempHolder = EditorGUILayout.IntSlider(tempHolder, 0, 100);
+    }
+  }
+}
